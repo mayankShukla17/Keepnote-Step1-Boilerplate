978561bafa5728bcf56bd8c864fbf54f6ddef347
Mayank Shukla <mayankgonda@gmail.com>
2020-10-27 08:30:13 +0000

    Keepnote Level 1 Assignment Completed

--- src/main/java/com/stackroute/keepnote/model/Note.java
+++ src/main/java/com/stackroute/keepnote/model/Note.java
@@ -13,6 +13,11 @@ public class Note {
 	 * setters for the fields. The value of createdAt should not be accepted from
 	 * the user but should be always initialized with the system date
 	 */
+	private int noteId;
+	private String noteTitle;
+	private String noteContent;
+	private String noteStatus;
+	private LocalDateTime createdAt;
 
 	public Note() {
 	}
@@ -20,50 +25,96 @@ public class Note {
 	/* All the getters/setters definition should be implemented here */
 
 	public int getNoteId() {
-		return 0;
-
+		return noteId;
 	}
 
-	public void setNoteId(int intid) {
-
+	public void setNoteId(int noteId) {
+		this.noteId = noteId;
 	}
 
 	public String getNoteTitle() {
-		return null;
+		return noteTitle;
 	}
 
-	public void setNoteTitle(String string) {
-
+	public void setNoteTitle(String noteTitle) {
+		this.noteTitle = noteTitle;
 	}
 
 	public String getNoteContent() {
-		return null;
+		return noteContent;
 	}
 
-	public void setNoteContent(String string) {
-
+	public void setNoteContent(String noteContent) {
+		this.noteContent = noteContent;
 	}
 
 	public String getNoteStatus() {
-		return null;
+		return noteStatus;
 	}
 
-	public void setNoteStatus(String string) {
-
+	public void setNoteStatus(String noteStatus) {
+		this.noteStatus = noteStatus;
 	}
 
 	public LocalDateTime getCreatedAt() {
-		return null;
+		return createdAt;
 	}
 
-	public void setCreatedAt(LocalDateTime localdatetime) {
-
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
 	}
+	//	public int getNoteId() {
+	//		return 0;
+	//
+	//	}
+	//
+	//	public void setNoteId(int intid) {
+	//
+	//	}
+	//
+	//	public String getNoteTitle() {
+	//		return null;
+	//	}
+	//
+	//	public void setNoteTitle(String string) {
+	//
+	//	}
+	//
+	//	public String getNoteContent() {
+	//		return null;
+	//	}
+	//
+	//	public void setNoteContent(String string) {
+	//
+	//	}
+	//
+	//	public String getNoteStatus() {
+	//		return null;
+	//	}
+	//
+	//	public void setNoteStatus(String string) {
+	//
+	//	}
+	//
+	//	public LocalDateTime getCreatedAt() {
+	//		return null;
+	//	}
+	//
+	//	public void setCreatedAt(LocalDateTime localdatetime) {
+	//
+	//	}
+
+
 
 	/* Override the toString() method */
 
+	//	@Override
+	//	public String toString() {
+	//		return null;
+	//	}
 	@Override
 	public String toString() {
-		return null;
+		return "Note [noteId=" + noteId + ", noteTitle=" + noteTitle + ", noteContent=" + noteContent + ", noteStatus="
+				+ noteStatus + ", createdAt=" + createdAt + "]";
 	}
 }
